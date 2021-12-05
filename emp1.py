013cd7f7eab392db8f7812e323e9fbee3feea76f
Shubham4912 <shubhamsp3949@gmail.com>
2021-12-05 12:03:10 +0530

    first commit

---
+++ emp1.py
@@ -0,0 +1,253 @@
+import mang
+class emp:
+    def __init__(self):
+        self.emp_info={}
+        self.emp_leave=10
+        self.mang_data={}
+    def emp_id(self):
+        while True:          
+            try:
+                emp_id=int(input("Enter the employee id: "))
+                l=str(emp_id)
+                if len(l)<5:
+                    break
+                else:
+                    print("Enter employee id less than 5 digit")
+            except ValueError:
+                print("Plese enter valid Employee Id accept only digit")
+        return emp_id
+    def emp_name(self):
+        while True:
+            emp_name=input("Enter the employee name : ")
+            if emp_name.isalpha():
+                break
+            else:
+                print("please enter the valid name")
+        return emp_name
+    def emp_post(self):
+        print("Enter the employee position :\n 1.manger\n 2.Softer Engineer\n 3.Hardware Engineer\n 4.Trainee Engineer")
+        while True:
+            try:
+                op=int(input("Enter above option: "))
+                if op==1:
+                    emp_position="manger"
+                    break
+                elif op ==2:
+                    emp_position="software engineer"
+                    break
+                elif op ==3:
+                    emp_position="Hardware engineer"
+                    break
+                elif op==4:
+                    emp_position="Trainee engineer"
+                    break
+            except ValueError:
+                print("plase enter valid option")
+
+        return emp_position
+       
+
+    # Add employ information into dictionery
+    def add_emp(self):
+        id=self.emp_id()                     # get employee id
+        name=self.emp_name()                 # get employee name
+        emp_position=self.emp_post()         # get employee position
+        if not self.emp_info:
+            self.emp_info.update({id:{"name":name,"position":emp_position,"leave":self.emp_leave}})  #add information into dictionery according to employee Id
+        else:
+            if  id in self.emp_info.keys():
+                print("Enter employee id alredy exist")
+            else:
+                self.emp_info.update({id:{"name":name,"position":emp_position,"leave":self.emp_leave}})
+      #  print("Add employee successfully")
+        return self.emp_info
+    
+    #add employee as manger
+    def add_manger(self):
+        if not self.emp_info:
+            print("First add employee")
+        else:
+            print("Enter the ID of employee want to make as manger")
+            id=int(input())
+            if id in self.emp_info:
+                m1=self.emp_info.get(id)
+                m2=m1.get("position")
+                if m2 != "manger":
+                    m1["position"]="manger"
+                    #print(m1)
+                    return self.emp_info
+                else:
+                    print(f"{id} he is alradey manger")
+            
+            else:
+                print("Employee ID is invalid")
+    
+    #Assign employee under Manger 
+    def assingn_emp_under_mang(self):
+        if not self.emp_info:
+            print("First add employee")
+        else:
+            d=m.assign_emp_under_mang(self.emp_info)
+       #print(d)
+            return d
+    
+    # Remove employee
+    def rem_emp(self):
+        if not self.emp_info:
+            print("First add employee")
+        else:
+            print("Enter the employee id to remove: ")
+            id=int(input())
+            if id in self.emp_info:
+                del self.emp_info[id]
+                print("Remove employee Success")
+            else:
+                print("Id of employee in invalid")
+            
+            r=m.remove_emp(id)
+            return self.emp_info
+    #Remove employee as manger
+    def rem_manger(self):
+        if not self.emp_info:
+            print("First add employee")
+        else:
+            print("Enter the ID of employee want to remove as manger")
+            id=int(input())
+            if id in self.emp_info:
+                m1=self.emp_info[id]["position"]
+                if m1 == "manger":
+                    self.emp_info[id]["position"]="Senior Engineer"
+                    #return self.emp_info
+                else:
+                    print("he is not a manger")
+                    #return self.emp_info
+            else:
+                print("Employee ID is invalid")
+            r=m.rem_mang(id)
+            return self.emp_info
+    #Apply leave employee
+    def apply_leave(self):
+        if not self.emp_info:
+            print("First add employee")
+        else:
+            print("Enter Id which employee want leave")
+            id=int(input())
+            if id in self.emp_info:
+                l1=self.emp_info[id]["leave"]
+                c=0
+                while c<3:
+                    try:
+                        c +=1
+                        print("Avilable leave is: ",l1)
+                        print("How many day want to leave")
+                        l=int(input( ))
+                        if l>0:
+                            if l<=l1:
+                                print("resone want to leave\n 1. Sick leave\n 2. Casual Leave\n 3. optional leave\n")
+                                op=int(input( ))
+                                if op ==1:
+                                    a=m.approve_leave(id,"sick leave",l,self.emp_info)
+                                    #print(f"Employee {id} is apply for sick leave")
+                                    if a=="yes":
+                                        print("Leave is approved")
+                                        ave_l=l1-l
+                                        self.emp_info[id]["leave"]=ave_l
+                                        print("remening leave is: ",ave_l)
+                                        break
+                                       
+                                    elif a=="no":
+                                        print("Sorry,Leave is not approve")
+                                        break
+
+                                elif op==2:
+                                    a=m.approve_leave(id,"casual leave",l,self.emp_info)
+                                    if a=="yes":
+                                        print("Leave is approved")
+                                        ave_l=l1-l
+                                        self.emp_info[id]["leave"]=ave_l
+                                        print("remening leave is: ",ave_l)
+                                        break
+                                    
+                                    elif a=="no":
+                                        print("Sorry,Leave is not approve")
+                                        break
+
+                                elif op==3:
+                                    a=m.approve_leave(id,"optional leave",l,self.emp_info)
+                                    if a=="yes":
+                                        print("Leave is approved")
+                                        ave_l=l1-l
+                                        self.emp_info[id]["leave"]=ave_l
+                                        print("remening leave is: ",ave_l)
+                                        break
+                                    elif a=="no":
+                                        print("Sorry,Leave is not approve")
+                                        break
+                            else:
+                                print("you don't have that much leave ")
+                        else:
+                            print("please Apply for more than 1 leave ")
+                    except ValueError:
+                        print("please try again")
+                return self.emp_info
+            else:
+                print("You Enter wrong Employee Id")
+    #see information of employee
+    def print_info(self):
+        if not self.emp_info:
+            print("First add employee")
+        else:
+            das="-"*70
+            print(das)
+            print("{:<15}|  {:<15}|  {:<20}|  {:<10}|".format("Employee Id","Name","position","leave"))
+            das="-"*70
+            print(das)
+            for k,v in self.emp_info.items():
+                name=v["name"]
+                position=v["position"]
+                leave=v["leave"]
+            #  print(name,position,leave)
+                print ("{:<15}|  {:<15}|  {:<20}|  {:<10}|".format(k, name,position,leave))
+            das="-"*70
+            print(das)
+
+if __name__=="__main__":
+    e=emp()
+    m=mang.manger()
+    while True:
+        print("Enter the option\n 1. add employee\n 2. remove employee\n 3. make employee as manger\n 4. remove employee from manger\n 5. Assingn employee under RA manger\n 6. apply leave\n 7. see information employee data\n 8. see employee assign info\n 9. End program ")  
+        try:
+            option=int(input("Enter option "))
+            if option == 1:
+                e1=e.add_emp()
+                #print(e1)
+                e.print_info()
+            elif option ==2:
+                e1=e.rem_emp()
+                #print(e1)
+                e.print_info()
+            elif option ==3:
+                e1=e.add_manger()
+                e.print_info()
+                #print(e1)
+            elif option ==4:
+                e5=e.rem_manger()
+                e.print_info()
+               # print(e5)
+            elif option==5:
+                e6=e.assingn_emp_under_mang()
+                m.print_info()
+            elif option ==6:    
+                e1=e.apply_leave()
+                e.print_info()
+                #print(e1)
+            elif option ==7:
+                e.print_info()
+            elif option ==8:
+                m.print_info()
+            elif option ==9:
+                break
+            else :
+                print("Enter the valid option")
+        except ValueError:
+            print("Enter the valid option option")
---
+++ mang.py
@@ -0,0 +1,269 @@
+#Assign employee under manger
+
+from emp1 import*
+class manger():
+    def __init__(self):
+        self.mang_data={}
+    def  assign_emp_under_mang(self,data):
+        if not self.mang_data:
+            print("check employee data")
+            mang=[]
+            emp=[]
+            for k in data.keys():
+                if data[k]["position"]=="manger":
+                    mang.append(k)
+                else:
+                    emp.append(k)
+            print(f"Manger are{mang}")
+            print(f"Employees are{emp}")
+            if len(mang)==0:
+                print("Manger is not avilable fist add manger")
+            else:
+                print("Enter the manger id want to assign employee")
+                mang_id=int(input())
+                if mang_id in mang:
+                    mang.remove(mang_id)
+                    print(f"Enter employee id want to assign under manger ({mang_id})") 
+                    emp_id=int(input())
+                    if emp_id in emp:
+                        emp.remove(emp_id)
+                        d1=data.get(emp_id)
+                        self.mang_data[mang_id]=[{emp_id:d1}] 
+                while True:
+                    if len(emp)!=0:
+                        print(f"Do you want to assign more employee under same manger press y else assign employee under another manger press o ")
+                        op=input()
+                        if op=="y":
+                            print("avilable employee: ",emp)
+                            emp_id=int(input(f"Enter employee id want to assign  under manger({mang_id}) : "))
+                            if emp_id in emp:
+                                emp.remove(emp_id)
+                                d1=data.get(emp_id)
+                                l=self.mang_data.get(mang_id)
+                                l.append({emp_id:d1})
+                                if len(emp)!=0:
+                                    continue
+                                else:
+                                    break
+                            else:
+                                print("enter the wrong employee id please try again")
+                                break
+                        elif op =="o":
+                            print("avilable employee: ",emp)
+                            print("Enter the manger id want to assign employee")
+                            mang_id2=int(input())
+                            if mang_id2 in mang :  
+                                mang.remove(mang_id2) 
+                                print(f"Enter employee id want to assign under manger ({mang_id2})") 
+                                emp_id=int(input())
+                                if emp_id in emp:
+                                    emp.remove(emp_id)
+                                    d1=data.get(emp_id)
+                                    self.mang_data[mang_id2]=[{emp_id:d1}]
+                                    
+                                    while True:
+                                        if len(emp)!=0:
+                                            print(f"Do you want to assign more employee under same manger press y else press n")
+                                            op=input()
+                                            if op=="y":
+                                                print("avilable employee: ",emp)
+                                                emp_id=int(input(f"Enter employee id want to assign  under manger({mang_id2}) : "))
+                                                if emp_id in emp:
+                                                    emp.remove(emp_id)
+                                                    d1=data.get(emp_id)
+                                                    l=self.mang_data.get(mang_id2)
+                                                    l.append({emp_id:d1})
+                                                    if len(emp)!=0:
+                                                        continue
+                                                    else:
+                                                        break
+                                                    
+                                            else:
+                                                break
+                                        else:
+                                            break
+                            else:
+                                print("Enter the wrong manger id")
+                                break
+                        else:
+                            print("Enter the wrong option")
+                            break
+                    
+                    else:
+                        print("employee is assign undr RA manger")
+                        break
+                return self.mang_data
+        else:
+            self.s=self.assign_emp(data)
+            return self.s
+
+    def assign_emp(self,data):
+        
+        mang=[]
+        emp=[]
+        for k in data.keys():
+            if data[k]["position"]=="manger":
+                mang.append(k)
+            else:
+                emp.append(int(k))
+       # print("check employee data",emp)
+        print(" manger is",mang)
+        mang1=[]
+        emp1=[]
+        for k,v in self.mang_data.items():
+            mang1.append(k)
+            for i in v:
+                for j in i:
+                    emp1.append(int(j))
+        #print("check employee data",emp1)
+        #print("check manger data",mang1)
+                
+        emp2=[]
+        for ele in emp:
+            if ele not in emp1:
+                emp2.append(ele)
+
+        print("employee remening to assign under manger is ",emp2)
+
+        print("enter manger id want to assing employee")
+        man_id=int(input())
+        if man_id in mang1 :
+            print("enter the employee id")
+            emp_id=int(input())
+            if emp_id in emp2:
+                emp2.remove(emp_id)
+                d1=data.get(emp_id)
+                l=self.mang_data.get(man_id)
+                l.append({emp_id:d1})
+                
+        elif man_id in mang :
+            print("enter the employee id")
+            emp_id=int(input())
+            if emp_id in emp2:
+                emp2.remove(emp_id)
+                d1=data.get(emp_id)
+                self.mang_data[man_id]=[{emp_id:d1}]
+        else:
+            print("Enter the wrong id" )
+        return self.mang_data
+
+    def approve_leave(self,id,res,l,data):
+      #  print(f"Employee {id} is apply for {res}")
+        mang=[]
+        emp=[]
+        for k,v in data.items():
+            if data[k]["position"]=="manger":
+                mang.append(k)
+            else:
+                emp.append(k)
+   
+        mang1=[]
+        emp1=[]
+        for k,v in self.mang_data.items():
+            mang1.append(k)
+            for i in v:
+                for j in i:
+                    emp1.append(int(j))
+        if id in emp1:
+           # print(f"Employee {id} is apply for {res}")
+            print(f"Employee {id} apply for {l} day of {res}")
+            c=0
+            for k,v in self.mang_data.items():
+                for i in v:
+                    e=self.mang_data[k][c]
+                    c+=1
+                    for j in i:
+                        if id ==int(j):
+                            print(f"Employee {id} working under {k} manger")
+                            #check employee leave
+                            print("Employee avilable leave are",e[id]["leave"])
+                            l2=e[id]["leave"]
+                            if l2>=l:
+                                print(f"manger({k}) approve or reject a leave")
+                                dec=input()
+                                if dec=="yes":
+                                    l3=l2-l
+                                    e[id]["leave"]=l3
+                                   # print("Manger data: ",self.mang_data)
+                                    return "yes"
+                                elif dec =="no":
+                                    return "no"
+                c=0       
+        elif id in mang:
+            print(f"Manger ({id}) apply for {l} day of {res}")
+            print("manger avilable leave are",data[id]["leave"])
+            l2=data[id]["leave"]
+            if l2>=l:
+                print("Enter approve or reject a leave")
+                dec=input()
+                if dec=="yes":
+                    l3=l2-l
+                    data[id]["leave"]=l3
+                    # print("Manger data: ",self.mang_data)
+                    return "yes"
+                elif dec =="no":
+                    return "no"
+   
+        elif id in emp:
+            print("Employee is not assign under manger first assign Ra manger")
+            return "no"
+
+    def remove_emp(self,id):
+        mang1=[]
+        emp1=[]
+        for k,v in self.mang_data.items():
+            mang1.append(k)
+            for i in v:
+                for j in i:
+                    emp1.append(int(j))
+        if id in emp1:
+            #print(f"Employee {id} is removed")
+            c=0
+            for k,v in self.mang_data.items():
+                for i in v:
+                    e=self.mang_data[k][c]
+                    c+=1
+                    for j in i:
+                        if id ==int(j):
+                            del e[j]
+                            return self.mang_data
+                c=0            
+
+
+        elif id in mang1:
+            del self.mang_data[id] 
+            return self.mang_data
+        else:
+            pass
+
+    def rem_mang(self,id):
+        if id in self.mang_data:
+            del self.mang_data[id]
+            return self.mang_data
+
+    def print_info(self):
+        if not self.mang_data:
+            print("Not add any employee under manger")
+        else:
+            c=0
+            for k,v1 in self.mang_data.items():
+                print(f"Employee Working under manger {k} is: ")
+                das="-"*63
+                print(das)
+                print("{:<15}|  {:<10}|  {:<18}|  {:<10}".format("Employee Id","Name","position","leave"))
+                das="-"*63
+                print(das)
+                for i in v1:
+                    d=self.mang_data[k][c]
+                    c=c+1
+                    for j in i:
+                        s=int(j)
+                        name=d[s]["name"]
+                        position=d[s]["position"]
+                        leave=d[s]["leave"]
+                        
+                        print ("{:<15}|  {:<10}|  {:<18}|  {:<10}".format(s, name,position,leave))
+                das="-"*60
+                print(das)
+
+            c=0
